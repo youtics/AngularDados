9c096ec68e1428743f4ab74aa773ed92d5abe9b9
youtics <youtics@gmail.com>
2022-07-07 14:57:46 -0300

    Dados terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  dadoIzquierda:string = '../assets/img/dice1.png';
+  dadoDerecha:string = '../assets/img/dice4.png';
+  numero1:number=1;
+  numero2:number=1;
+  gano:boolean=false;
+
+
+  tirarDados():void{
+    this.numero1 = Math.round((Math.random()*5)+1);
+    this.numero2 = Math.round((Math.random()*5)+1);
+    this.dadoDerecha = '../assets/img/dice'+this.numero1+'.png';
+    this.dadoIzquierda = '../assets/img/dice'+this.numero2+'.png';
+    if(this.numero1==this.numero2)
+      this.gano = true;
+      else
+      this.gano = false;
+  }
 }
